fbad31455dad3f56d594334c0eb388e946fdea38
mhio <37205532+mhio@users.noreply.github.com>
2020-04-03 14:48:23 +1100

    Add int koa test

--- test/int/MhioLoggerHttp.spec.js
+++ test/int/MhioLoggerHttp.spec.js
@@ -1,4 +1,7 @@
 const { expect } = require('chai')
+const Koa = require('koa')
+const supertest = require('supertest')
+
 const { MhioLoggerHttp, noop } = require('../../lib/MhioLoggerHttp')
 
 describe('int::MhioLogger', function(){
@@ -10,6 +13,14 @@ describe('int::MhioLogger', function(){
   it('should instance', function(){
     const Logger = new MhioLoggerHttp('test')
     expect(Logger.pinoKoa()).to.be.a('function')
+  })
+
+  it('should run as middleware', async function(){
+    const app = new Koa()
+    app.use(MhioLoggerHttp.pinoKoa())
+    app.use(ctx => ctx.body = 'hello world')
+    const res = await supertest(app.callback()).get('/')
+    expect(res.body).to.eql({}) 
   })
 
 })
